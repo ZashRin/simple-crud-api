040f8c0dd9accaa2d2c26ce68ea33dbc14b48c8b
Alex <itgtktdbx@gmail.com>
2022-06-19 13:46:52 +0300

    refactor: quotes

--- src/services/server.service.ts
+++ src/services/server.service.ts
@@ -1,5 +1,5 @@
-import http from "http";
-import { IRequest } from "../models/server.interface";
+import http from 'http';
+import { IRequest } from '../models/server.interface';
 
 export type RouterCallbackFunction = (req: IRequest, res: http.ServerResponse, err?: any) => void;
 export type MethodType = 'GET' | 'PUT' | 'POST' | 'DELETE';
